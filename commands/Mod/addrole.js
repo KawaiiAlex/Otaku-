c697ac638d6eada06d85750f1551c0e327e354b5
KawaiiAlex <38259844+KawaiiAlex@users.noreply.github.com>
2018-04-21 14:14:12 +0200

    Update addrole.js

--- commands/Mod/addrole.js
+++ commands/Mod/addrole.js
@@ -2,23 +2,23 @@ const Discord = require("discord.js");
 exports.run = async (client, message, args) => {
 
   //!addrole @andrew Dog Person
-  if(!message.channel.permissionsFor(message.author).has("MANAGE_ROLES")) return message.reply("Désolé, tu ne peux pas faire ça.");
-  if(!message.channel.permissionsFor(client.user).has("MANAGE_ROLES")) return message.reply("Désolé, je ne peux pas faire ça.");
+  if(!message.channel.permissionsFor(message.author).has("MANAGE_ROLES")) return message.reply("Désolé, tu ne peux pas faire ça.").then(messages => message.channel.bulkDelete(messages));
+  if(!message.channel.permissionsFor(client.user).has("MANAGE_ROLES")) return message.reply("Désolé, je ne peux pas faire ça.").then(messages => message.channel.bulkDelete(messages));
   let rMember = message.guild.member(message.mentions.users.first()) || message.guild.members.get(args[0]);
-  if(!rMember) return message.reply("Impossible de trouver cet utilisateur.");
+  if(!rMember) return message.reply("Impossible de trouver cet utilisateur.").then(messages => message.channel.bulkDelete(messages));
   let role = args.join(" ").slice(22);
-  if(!role) return message.reply("Spécifiez un rôle!");
+  if(!role) return message.reply("Spécifiez un rôle!").then(messages => message.channel.bulkDelete(messages));
   let gRole = message.guild.roles.find(`name`, role);
-  if(!gRole) return message.reply("Je ne trouve pas ce rôle.");
+  if(!gRole) return message.reply("Je ne trouve pas ce rôle.").then(messages => message.channel.bulkDelete(messages));
 
-  if(rMember.roles.has(gRole.id)) return message.reply("Il a déja ce rôle.");
+  if(rMember.roles.has(gRole.id)) return message.reply("Il a déja ce rôle.").then(messages => message.channel.bulkDelete(messages));
   await(rMember.addRole(gRole.id));
 
   try{
     await rMember.send(`Félicitations, vous avez reçu le rôle ${gRole.name}`)
     message.channel.send(`${rMember} a reçu le rôle ${gRole.name}.`)
   }catch(e){
-    message.channel.send(`<@${rMember.id}> a reçu le rôle **${gRole.name}**. J'ai essayé de le MP, mais ces MP sont bloqués.`)
+    message.channel.send(`<@${rMember.id}> a reçu le rôle **${gRole.name}**. J'ai essayé de le MP, mais ces MP sont bloqués.`).then(messages => message.channel.bulkDelete(messages))
   }
 }
 
