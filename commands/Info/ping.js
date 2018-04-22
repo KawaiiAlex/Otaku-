e20314048f20d18627ebc214ce87b3568d895ce1
KawaiiAlex <38259844+KawaiiAlex@users.noreply.github.com>
2018-04-22 11:13:59 +0200

    Update ping.js

--- commands/Info/ping.js
+++ commands/Info/ping.js
@@ -7,7 +7,7 @@ const m = await message.channel.send("Ping?");
 
         var PingEmbed = new Discord.RichEmbed()
 .setColor("#689AFB")
-.addField("Pong !", `**${m.createdTimestamp - message.createdTimestamp}ms**`)
+.addField('Pong !', '***' + `${Date.now() - message.createdTimestamp}` + ' ms***')
 .setFooter(`Ping`).setTimestamp();
 
 m.edit(PingEmbed);
