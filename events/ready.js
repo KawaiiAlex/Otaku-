ec0742f1e232d740aebb3fa043c494dc36378733
KawaiiAlex <38259844+KawaiiAlex@users.noreply.github.com>
2018-04-22 15:18:53 +0200

    Update ready.js

--- events/ready.js
+++ events/ready.js
@@ -4,9 +4,10 @@ const purplecolor = chalk.keyword('purple');
 
 module.exports = client => { // eslint-disable-line no-unused-vars
   console.log(purplecolor(`${client.user.username} en ligne. \n` + 'Connecté dans:\n' + client.guilds.array()));
- // client.user.setActivity(`${client.users.size} utilisateurs et ${client.guilds.size} serveurs || //help`, {type: "WATCHING"})
-  
+ client.user.setActivity(`${client.users.size} utilisateurs et ${client.guilds.size} serveurs || //help`, {type: "WATCHING"})
+  /*
   client.user.setActivity(`${client.users.username} en modification`, {type: "WATCHING"});
   client.user.setStatus( 'idle' );
+  */
   
 };
