912d5015ab59d4e498bf94cebb6b00fecbedf5ca
KawaiiAlex <38259844+KawaiiAlex@users.noreply.github.com>
2018-04-24 17:05:53 +0200

    Update lockdown.js

--- commands/Admin/lockdown.js
+++ commands/Admin/lockdown.js
@@ -1,17 +1,17 @@
 const ms = require('ms');
 exports.run = (client, message, args) => {
-  /*
   if (!client.lockit) client.lockit = [];
   const time = args.join(' ');
   const validUnlocks = ['release', 'unlock'];
-  if (!time) return message.reply('Vous devez définir une durée pour le lockdown. En heures, minutes ou secondes');
+  if (!time) return message.reply('Vous devez dire un temp avec ``s``, ``m``, ``h``, ``d``');
 
   if (validUnlocks.includes(time)) {
     message.channel.overwritePermissions(message.guild.id, {
       SEND_MESSAGES: null
     }).then(() => {
-      message.channel.send('Lockdown levé');
+      message.channel.send('Channel Débloqué');
       clearTimeout(client.lockit[message.channel.id]);
+      delete client.lockit[message.channel.id];
     }).catch(error => {
       console.log(error);
     });
@@ -19,12 +19,13 @@ exports.run = (client, message, args) => {
     message.channel.overwritePermissions(message.guild.id, {
       SEND_MESSAGES: false
     }).then(() => {
-      message.channel.send(`Channel lock pendant ${ms(ms(time), { long:true })}`).then(msg => {msg.delete(ms(time))}).then(() => {
+      message.channel.send(`Salon bloqué pendant ${ms(ms(time), { long:true })}`).then(() => {
 
         client.lockit[message.channel.id] = setTimeout(() => {
           message.channel.overwritePermissions(message.guild.id, {
             SEND_MESSAGES: null
-          }).then(message.channel.send('Lockdown levé')).then(msg => {msg.delete(10000)});
+          }).then(message.channel.send('Channel débloqué')).catch(console.error);
+          delete client.lockit[message.channel.id];
         }, ms(time));
 
       }).catch(error => {
@@ -32,8 +33,6 @@ exports.run = (client, message, args) => {
       });
     });
   }
-  */
-  message.channel.send("Commande en réparation. Désolé.")
 };
 exports.conf = {
   enabled: true,
