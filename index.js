61b4ff6f5bceadb0ec6b46cca969bd0ece0c61a9
KawaiiAlex <38259844+KawaiiAlex@users.noreply.github.com>
2018-04-25 20:35:41 +0200

    Update index.js

--- index.js
+++ index.js
@@ -132,5 +132,52 @@ client.on('error', e => {
   console.log(chalk.bgRed(e.replace(regToken, 'qui a été expurgé')));
 });
 
+var active = false
+ 
+client.on('ready', () => {
+  console.log('I am ready!');
+});
+ 
+/* client.on('message', message => {
+  if (message.content === '!Initialize') {
+    client.createRole('PUGS');
+    message.channel.send('bot initialized!')
+  }
+ });
+*/
+client.on('message', message => {
+  if (message.content === '//enable') {
+    message.channel.send('enabling...');
+    active = true
+    message.channel.send('AutoRole successfully enabled!')
+  }
+});
+ 
+client.on('message', message => {
+  if (message.content === '//disable') {
+    message.channel.send('disabling...');
+    active = false
+    message.channel.send('AutoRole successfully disabled!')
+  }
+});
+ 
+client.on('message', message => {
+  if (message.content === '//check') {
+    switch (active) {
+      case true:
+        message.channel.send('Addon currently ACTIVE');
+        break;
+      case false:
+        message.channel.send('addon currently DEACTIVE');
+        break;
+                  }
+}});
+ 
+client.on('guildMemberAdd', member => {
+  if(active === true) {
+    var role = member.guild.roles.find("name", "Atarashii'Otaku");
+    member.addRole(role).catch(console.error);
+  }
+})
 
 client.login(process.env.TOKEN)
