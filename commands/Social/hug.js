eaf0d6ca038a532411ec4c0076fe5801632a26e6
KawaiiAlex <38259844+KawaiiAlex@users.noreply.github.com>
2018-04-28 20:15:03 +0200

    Update hug.js

--- commands/Social/hug.js
+++ commands/Social/hug.js
@@ -4,7 +4,7 @@ const Discord = require('discord.js');
 const fs = require("fs");
 exports.run = (client, message, args) => {
 
-  let defineduser = message.mentions.users.first();
+ /* let defineduser = message.mentions.users.first();
 
   fs.readFile('./Link/hug.txt', 'utf8', function(err, data) {
       if (err) throw err;
@@ -21,7 +21,8 @@ exports.run = (client, message, args) => {
   .setFooter("hug");
 
   message.channel.send(HugEmbed)
-});
+});*/
+ message.channel.send("Commande en cours d'amélioration");
 }
 
   
