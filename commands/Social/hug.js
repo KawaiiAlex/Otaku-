788aa720575588a52c4be9324c4aa58fb8a7d049
KawaiiAlex <38259844+KawaiiAlex@users.noreply.github.com>
2018-04-29 11:45:26 +0200

    Add files via upload

---
+++ commands/Social/hug.js
@@ -0,0 +1,40 @@
+const Discord = require('discord.js');
+const fs = require("fs");
+exports.run = (client, message, args) => {
+  function getRandomInt(max) {
+    return Math.floor(Math.random() * (max + 1));
+}
+
+  let defineduser = message.mentions.users.first();
+
+  fs.readFile('./Link/hug.txt', 'utf8', function(err, data) {
+      if (err) throw err;
+      var random = data.split('\n');
+      var num = getRandomInt(random.length);
+      var url = random[num];
+
+  
+
+      var HugEmbed = new Discord.RichEmbed()
+  .setColor("#689AFB")
+  .setTitle(`${message.author.username} fait un hug à ${defineduser.username}`)
+  .setImage(url)
+  .setFooter("hug");
+
+  message.channel.send(HugEmbed)
+});
+}
+
+  
+  exports.conf = {
+    enabled: true,
+    guildOnly: false,
+    aliases: [],
+    permLevel: 0
+  };
+  
+  exports.help = {
+    name: 'hug',
+    description: 'Pour faire des calîns au personne que vous souhaitez',
+    usage: 'hug [mention ou mot]'
+  };
