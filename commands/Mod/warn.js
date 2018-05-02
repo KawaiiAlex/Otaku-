21d0eb2f0f144cb4a656c06c38853f3cb07d7b35
KawaiiAlex <38259844+KawaiiAlex@users.noreply.github.com>
2018-05-02 22:12:05 +0200

    Update warn.js

--- commands/Mod/warn.js
+++ commands/Mod/warn.js
@@ -4,7 +4,7 @@ const {parseUser} = require('../../util/parseUser.js');
 const settings = require('../../config.json');
 const fs = require("fs");
 exports.run = async (client, message, args) => {
-    /*
+
     if (message.author.bot) return;
   if(message.channel.type === "dm") return;
 
@@ -17,19 +17,18 @@ exports.run = async (client, message, args) => {
 let prefix = prefixes[message.guild.id].prefixes;
   const user = message.mentions.users.first();
   parseUser(message, user);
-  const modlog = client.channels.find('name', 'otaku-logs');
-  if (!modlog) return message.reply('Je ne trouve pas le channel otaku-logs.');
-  if (message.mentions.users.size < 1) return message.reply('Vous devez mentionner un utilisateur pour ').catch(console.error);
+  let incidentchannel = message.guild.channels.find(`name`, "otaku-logs");
+  if(!incidentchannel) return message.channel.send("Je ne trouve pas le channel``otaku-logs``. \n Créé le svp C:");
+  if (message.mentions.users.size < 1) return message.reply('Vous devez mentionner un utilisateur pour warn ').catch(console.error);
 
   const reason = args.splice(1, args.length).join(' ') || `Tu dois dire une raison pour warn.`;
   const embed = new RichEmbed()
   .setColor(0x00AE86)
   .setTimestamp()
   .setDescription(`**Action:** Warning\n**Utilisateur Warn:** ${user.tag}\n**Modérateur:** ${message.author.tag}\n**Raison:** ${reason}`)
-  return client.channels.get(modlog.id).send({embed});
+  return client.channels.get(incidentchannel.id).send({embed});
   return message.channel.send(":white_check_mark: **| Cette utilisateur c'est fait warn.**");
-    */
-    message.channel.send("Commande inutilisable pour le moment");
+
 };
 
 
