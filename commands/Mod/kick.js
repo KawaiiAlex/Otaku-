7e36f124d8ad652b547e90d11361d6bb57bc8336
KawaiiAlex <38259844+KawaiiAlex@users.noreply.github.com>
2018-05-02 21:38:51 +0200

    Update kick.js

--- commands/Mod/kick.js
+++ commands/Mod/kick.js
@@ -6,11 +6,10 @@ exports.run = (client, message, args) => {
 
     if(!message.channel.permissionsFor(message.author).has("KICK_MEMBERS")) return message.channel.send("Tu n'as pas les droits").then(msg => {msg.delete(5000)});;
     if(!message.channel.permissionsFor(client.user).has("KICK_MEMBERS")) return message.channel.send("Je n'ai pas les droits").then(msg => {msg.delete(5000)});;
-
+     parseUser(message, user);
     let kUser = message.guild.member(message.mentions.users.first() || message.guild.members.get(args[0]));
     if(!kUser) return message.channel.send("Je ne trouve pas cette utilisateur").then(msg => {msg.delete(5000)});;
     let kReason = args.join(" ").slice(22);
-    parseUser(message, user);
 
     let kickEmbed = new Discord.RichEmbed()
     .setDescription("~Kick~")
