ae355a79ceef592dfb6c73f3d41bd76d3e69e9f3
KawaiiAlex <38259844+KawaiiAlex@users.noreply.github.com>
2018-05-10 20:04:09 +0200

    Update rping.js

--- commands/test/rping.js
+++ commands/test/rping.js
@@ -1,1 +1,30 @@
+  const Discord = require('discord.js');
 
+exports.run = async (client, message) => {
+
+
+/*const m = await message.channel.send("Ping?");
+
+        var PingEmbed = new Discord.RichEmbed()
+.setColor("#689AFB")
+.addField('Pong !', '***' + `${new Date().getTime() - message.createdTimestamp}` + ' ms***')
+.setFooter(`Ping`).setTimestamp();
+
+m.edit(PingEmbed);
+}*/
+message.channel.send(`= Pong =\n\n${new Date().getTime() - message.createdTimestamp} ms`, {code:'asciidoc'});
+                     };
+  
+  
+  exports.conf = {
+    enabled: true,
+    guildOnly: false,
+    aliases: [],
+    permLevel: 0
+  };
+  
+  exports.help = {
+    name: 'rping',
+    description: 'Ping/Pong command, vous donne aussi votre ping.',
+    usage: 'ping'
+  };
