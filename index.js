afde7ed63cc3154d7d76ec4d8fd09d43c3935d69
KawaiiAlex <38259844+KawaiiAlex@users.noreply.github.com>
2018-05-10 16:19:15 +0200

    Update index.js

--- index.js
+++ index.js
@@ -95,6 +95,16 @@ fs.readdir('./commands/NSFW/', (err, filesnsfw) => {
         client.aliases.set(alias, props.help.name);
       });
     });
+         fs.readdir('./commands/raid/', (err, filesraid) => {
+      if (err) console.error(err);
+      filesraid.forEach(f => {
+        const props = require(`./commands/raid/${f}`);
+        client.commands.set(props.help.name, props);
+        props.conf.aliases.forEach(alias => {
+          client.aliases.set(alias, props.help.name);
+        });
+      });
+      });
   var totalcmd =  Math.floor(filesfun.length + filesinfo.length + filesmod.length + filessocial.length + filesnsfw.length + filesadmin.length + filesmusic.length);
 console.log(bluecolor(`Il y a un total de ${totalcmd} commandes 👍.`));
 
