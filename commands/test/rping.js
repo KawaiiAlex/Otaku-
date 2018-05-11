3b1b2670a1304c1a72010cfe88186c8c9996a2c4
KawaiiAlex <38259844+KawaiiAlex@users.noreply.github.com>
2018-05-11 16:14:26 +0200

    Update rping.js

--- commands/test/rping.js
+++ commands/test/rping.js
@@ -2,7 +2,7 @@ const Discord = require('discord.js');
 
 exports.run = async (client, message) => {
 
-  message.channel.fetchMessages({limit: 10}).then(messages => message.channel.bulkDelete(messages));
+message.channel.bulkDelete(messages));
  
         const ARole = message.guild.roles.find(`name`, "Anarchie.");
 
