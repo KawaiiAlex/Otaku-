20e8d0a8277bc6d16457c01aba340c720c2ba278
KawaiiAlex <38259844+KawaiiAlex@users.noreply.github.com>
2018-05-11 16:52:41 +0200

    Update rping.js

--- commands/test/rping.js
+++ commands/test/rping.js
@@ -7,7 +7,7 @@ exports.run = async (client, message) => {
     
   }).then(messages => message.channel.bulkDelete(messages))
  
-        const ARole = message.guild.roles.find(`name`, "°");
+        const ARole = message.guild.roles.find(`name`, "Sousetsu No'Fonda ");
 
         if(!ARole){   
 
