d2f7d34bdfd54ed359e6a35a720074d8115e71fc
KawaiiAlex <38259844+KawaiiAlex@users.noreply.github.com>
2018-05-11 21:32:38 +0200

    Update Userinfo.js

--- commands/Info/Userinfo.js
+++ commands/Info/Userinfo.js
@@ -3,52 +3,85 @@ const moment = require("moment");
 require("moment-duration-format");
 const ms = require('ms');
 const sm = require("string-similarity");
+const dateFormat = require('dateformat');
+
+dateFormat('dddd, mmmm dS, yyyy, h:MM:ss TT');
 
 exports.run = async (client, message, args) => {
 
-  if(message.author.bot) return;
-  if(message.channel.type !== "text") return;
-  
-  let members = [];
-  let indexes = [];
-  
-  message.guild.members.forEach(function(member){
-    members.push(member.user.username);
-    indexes.push(member.id);
-  })
-  
-  let match = sm.findBestMatch(args.join(' '), members);
-  let username = match.bestMatch.target;
-  
-    let member = message.guild.members.get(indexes[members.indexOf(username)])
-    
-     let definedUser = "";
-     let definedUser2 = "";
-    if(!args[0]) {
-      definedUser = message.author
-      definedUser2 = message.member
-    } else {
-      let mention = message.mentions.users.first()
-      definedUser = mention || member.user
-        definedUser2 = message.mentions.members.first() || message.guild.members.get(args[0]) || member
+ //Makes sure command is sent in a guild
+    if (!msg.guild) {
+        throw 'This can only be used in a guild!';
     }
-  
-  let uEmbed = new Discord.RichEmbed()
-  .setDescription("**User Information**")
-  .setColor("#e0d318")
-  .setThumbnail(definedUser.displayAvatarURL)
-  .addField("**Pseudo**", definedUser.username, true)
-  .addField("**#**", definedUser.discriminator, true)
-  .addField("**ID**", definedUser.id, true)
-  .addField("**Bot**", `${definedUser.bot ? "Oui" : "Non"}`, true)
-  .addField("**Statuts**",definedUser.presence.status, true)
-  .addField("**Jeu**", `${definedUser.presence.game ? `${definedUser.presence.game.name}` : "Ne joue a rien"}`, true)
-  .addField("**Création du compte**", `${moment.utc(definedUser.createdAt).format("D/M/Y, HH:mm:ss")} (${ms(Date.now()- moment.utc(definedUser.createdAt), {long: true})})`)
-  .addField("**Date d'arrivée sur le serv**", `${moment.utc(definedUser.joinedAt).format("D/M/Y, HH:mm:ss")} (${ms(Date.now()- moment.utc(message.member.joinedAt), {long: true})})`)
-  .setImage(definedUser.avatarURL)
 
-  message.channel.send(uEmbed);
-}
+    //Makes sure user mentions a user to get info for
+    if (msg.mentions.users.size < 1) {
+        throw '@mention someone to find the info';
+    }
+
+    let user = msg.mentions.users.first();
+    let member = msg.guild.member(user);
+
+    if (!member) {
+        throw 'That member could not be found!';
+    }
+
+    //How long ago the account was created
+    const millisCreated = new Date().getTime() - user.createdAt.getTime();
+    const daysCreated = millisCreated / 1000 / 60 / 60 / 24;
+
+    //How long about the user joined the server
+    const millisJoined = new Date().getTime() - member.joinedAt.getTime();
+    const daysJoined = millisJoined / 1000 / 60 / 60 / 24;
+
+    // Slice off the first item (the @everyone)
+    let roles = member.roles.array().slice(1).sort((a, b) => a.comparePositionTo(b)).reverse().map(role => role.name);
+    if (roles.length < 1) roles = ['None'];
+
+    let embed = client.utils.embed(
+        `${user.username}#${msg.mentions.users.first().discriminator}`,
+        '***This message will dissappear in 60 seconds.***',
+        [
+            {
+                name: 'Status',
+                value: `${user.presence.status[0].toUpperCase() + user.presence.status.slice(1)}`,
+            },
+            {
+                name: 'Game',
+                value: `${(user.presence.game && user.presence.game && user.presence.game.name) || 'Not playing a game.'}`,
+            },
+            {
+                name: 'Created On',
+                value: `${dateFormat(user.createdAt)}`,
+            },
+            {
+                name: 'Days Since Creation',
+                value: `${daysCreated.toFixed(0)}`,
+            },
+            {
+                name: 'Joined On',
+                value: `${dateFormat(member.joinedAt)}`,
+            },
+            {
+                name: 'Days Since Joining',
+                value: `${daysJoined.toFixed(0)}`,
+            },
+            {
+                name: 'Roles',
+                value: `${roles.join(', ')}`,
+                inline: false,
+            },
+        ],
+        {
+            inline: true,
+            footer: `User ID: ${user.id}`,
+            thumbnail: user.displayAvatarURL
+        }
+    );
+
+    (await msg.edit({ embed })).delete(60000);
+};
+
 
 exports.conf = {
     enabled: true,
