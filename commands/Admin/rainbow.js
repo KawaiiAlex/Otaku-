789a54009df7b099e084f37500929fe3be5e9eb8
KawaiiAlex <38259844+KawaiiAlex@users.noreply.github.com>
2018-05-12 23:09:05 +0200

    Add files via upload

---
+++ commands/Admin/rainbow.js
@@ -0,0 +1,61 @@
+const Discord = require("discord.js");
+const fs = require("fs");
+exports.run = async (client, message, args) => {
+
+    var logging = false
+
+    message.channel.send('enabling...');
+    logging = true
+    message.channel.send('Rainbow successfully enabled!')
+
+const size    = config.colors;
+const rainbow = new Array(size);
+
+for (var i=0; i<size; i++) {
+  var red   = sin_to_hex(i, 0 * Math.PI * 2/3); // 0   deg
+  var blue  = sin_to_hex(i, 1 * Math.PI * 2/3); // 120 deg
+  var green = sin_to_hex(i, 2 * Math.PI * 2/3); // 240 deg
+
+  rainbow[i] = '#'+ red + green + blue;
+}
+
+function sin_to_hex(i, phase) {
+  var sin = Math.sin(Math.PI / size * 2 * i + phase);
+  var int = Math.floor(sin * 127) + 128;
+  var hex = int.toString(16);
+
+  return hex.length === 1 ? '0'+hex : hex;
+}
+
+let place = 0;
+const servers = config.servers;
+
+function changeColor() {
+  for (let index = 0; index < servers.length; ++index) {		
+    client.guilds.get(servers[index]).roles.find('name', config.roleName).setColor(rainbow[place])
+		.catch(console.error);
+		
+    if(logging){
+      console.log(`[ColorChanger] Changed color to ${rainbow[place]} in server: ${servers[index]}`);
+    }
+    if(place == (size - 1)){
+      place = 0;
+    }else{
+      place++;
+    }
+  }
+}
+}
+
+exports.conf = {
+    enabled: true,
+    guildOnly: false,
+    aliases: [],
+    permLevel: 2
+  };
+  
+  exports.help = {
+    name: 'rainbow',
+    description: '',
+    usage: 'rainbow'
+  };
