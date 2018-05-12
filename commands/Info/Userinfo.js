b307e5f2911fb2fc4ee2801ec6633ab15ba39888
KawaiiAlex <38259844+KawaiiAlex@users.noreply.github.com>
2018-05-12 20:23:15 +0200

    Update Userinfo.js

--- commands/Info/Userinfo.js
+++ commands/Info/Userinfo.js
@@ -38,6 +38,9 @@ exports.run = async (client, message, args) => {
     const millisJoined = new Date().getTime() - member.joinedAt.getTime();
 const daysJoined = millisJoined / 1000 / 60 / 60 / 24;
   
+     let roles = definedUser.roles.array().slice(1).sort((a, b) => a.comparePositionTo(b)).reverse().map(role => role.name);
+    if (roles.length < 1) roles = ['None'];
+  
   let uEmbed = new Discord.RichEmbed()
   .setDescription("**Informations Utilisateurs **")
   .setColor(`${message.guild.me.displayHexColor!=='#00000' ? message.guild.me.displayHexColor : 0xffffff}`)
@@ -49,7 +52,8 @@ const daysJoined = millisJoined / 1000 / 60 / 60 / 24;
   .addField("**Statuts**",definedUser.presence.status, false)
   .addField("**Jeu**", `${definedUser.presence.game ? `${definedUser.presence.game.name}` : "Ne joue a rien"}`, false)
   .addField("**Création du compte**", `${moment.utc(definedUser.createdAt).format("D/M/Y, HH:mm:ss")} ${daysCreated.toFixed(0)} jours`,false)
-  .addField("**Date d'arrivée sur le serv**", `${moment.utc(member.joinedAt).format("D/M/Y, HH:mm:ss")} ${daysJoined.toFixed(0)} jours`,false);
+  .addField("**Date d'arrivée sur le serv**", `${moment.utc(member.joinedAt).format("D/M/Y, HH:mm:ss")} ${daysJoined.toFixed(0)} jours`,false)
+   .addField("Roles", `${roles.join(', ')}`)
 
   message.channel.send(uEmbed);
 }
