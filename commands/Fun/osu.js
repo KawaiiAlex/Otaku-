612f3e22cd5d97abf20878c9b94abd53cdec1168
KawaiiAlex <38259844+KawaiiAlex@users.noreply.github.com>
2018-05-17 13:55:44 +0200

    Update osu.js

--- commands/Fun/osu.js
+++ commands/Fun/osu.js
@@ -1,9 +1,4 @@
-const osu = require('node-osu');
-const osuApi  = new osu.Api('a8fdf4f31ad6f6b0f8f4030d0942bc0411fa1511', {
-    // baseUrl: sets the base api url (default: https://osu.ppy.sh/api)
-    notFoundAsError: true, // Reject on not found instead of returning nothing. (default: true)
-    completeScores: true // When fetching scores also return the beatmap (default: false)
-});
+const {Attachment} = require('discord.js')
 const Discord = require("discord.js")
 
 exports.run = async (client, message, args) => {
