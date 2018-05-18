1ae4a62fe8ff4b430ea989a5637f48c35efecfa2
KawaiiAlex <38259844+KawaiiAlex@users.noreply.github.com>
2018-05-18 22:18:57 +0200

    Update ready.js

--- events/ready.js
+++ events/ready.js
@@ -4,7 +4,7 @@ const purplecolor = chalk.keyword('purple');
 
 module.exports = client => { // eslint-disable-line no-unused-vars
   console.log(purplecolor(`${client.user.username} en ligne. \n` + 'Connecté dans:\n' + client.guilds.array()));
- client.user.setActivity("//help pout plus d'info", 'https://www.twitch.tv/ryvalgaming')
+ client.user.setActivity("//help pour plus d'info", 'https://www.twitch.tv/ryvalgaming')
   client.user.setAvatar("https://cdn.discordapp.com/attachments/396314626565865482/447125319275773952/3d8f70a9d57f9cf514d4e06cda15c81e.png");
   /*
   client.user.setActivity(`${client.users.username} en modification`, {type: "WATCHING"});
@@ -12,4 +12,3 @@ module.exports = client => { // eslint-disable-line no-unused-vars
   */
 
 };
-
