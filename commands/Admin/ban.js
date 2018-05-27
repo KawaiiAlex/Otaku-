f1db0261e7822a32cc2fd0d99789fc1b2f9a5c4b
KawaiiAlex <38259844+KawaiiAlex@users.noreply.github.com>
2018-05-27 12:29:09 +0200

    Update ban.js

--- commands/Admin/ban.js
+++ commands/Admin/ban.js
@@ -7,7 +7,7 @@ exports.run = (client, message, args) => {
   let bUser = message.guild.member(message.mentions.users.first() || message.guild.members.get(args[0]));
   if(!bUser) return message.channel.send("Can't find user!");
   let bReason = args.join(" ").slice(22);
-  if(bUser.hasPermission("BAN_MEMBERS")) return message.channel.send("Tu ne peux pas le ban").then(msg => {msg.delete(5000)});
+  if (bUser.highestRole.position >= message.member.highestRole.position) return message.channel.send("Tu ne peux pas le ban").then(msg => {msg.delete(5000)});
   const user = message.mentions.users.first();   
 
   let banEmbed = new Discord.RichEmbed()
