3196a5589a1b1583db595a50497a3a9a8f1cb590
KawaiiAlex <38259844+KawaiiAlex@users.noreply.github.com>
2018-06-06 20:59:42 +0200

    Update ready.js

--- events/ready.js
+++ events/ready.js
@@ -5,16 +5,9 @@ const config = require('../config.json')
 const purplecolor = chalk.keyword('purple');
 
 module.exports = client => { // eslint-disable-line no-unused-vars
-    let prefixes = JSON.parse(fs.readFileSync("./prefixes.json", "utf8"));
-  if(!prefixes[message.guild.id]){
-    prefixes[message.guild.id] = {
-      prefixes: config.prefix
-    };
-  }
-  let prefix = prefixes[message.guild.id].prefixes;
   const ownerBot = client.users.get('281125214098685954').tag
   console.log(purplecolor(`${client.user.username} en ligne. \n` + 'Connecté dans:\n' + client.guilds.array()));
- client.user.setGame(prefix + ' help pour plus d\'info | ' + client.guilds.size() + ' serveurs | créé par ' + ownerBot, 'https://www.twitch.tv/ryvalgaming')
+ client.user.setGame('//help pour plus d\'info | ' + client.guilds.size() + ' serveurs | créé par ' + ownerBot, 'https://www.twitch.tv/ryvalgaming')
   client.user.setAvatar("https://cdn.discordapp.com/attachments/396314626565865482/447125319275773952/3d8f70a9d57f9cf514d4e06cda15c81e.png");
   /*
   client.user.setActivity(`${client.users.username} en modification`, {type: "WATCHING"});
