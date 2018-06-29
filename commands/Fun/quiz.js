0186e218a3081672af6b11862cbb39391d657e2b
KawaiiAlex <38259844+KawaiiAlex@users.noreply.github.com>
2018-06-29 21:05:50 +0200

    Update quiz.js

--- commands/Fun/quiz.js
+++ commands/Fun/quiz.js
@@ -1,26 +1,26 @@
 const Discord = require('discord.js');
 const quiz = [
-  { q: "𝑫𝒆 𝒒𝒖𝒆𝒍𝒍𝒆 𝒄𝒐𝒖𝒍𝒆𝒖𝒓 𝒆𝒔𝒕 𝒍𝒆 𝒄𝒊𝒆𝒍?", a: ["Pas de couleur", "invisible"] },
-  { q: "𝑵𝒐𝒎𝒎𝒆𝒛 𝒖𝒏𝒆 𝒎𝒂𝒓𝒒𝒖𝒆 𝒅𝒆 𝒃𝒐𝒊𝒔𝒔𝒐𝒏 𝒈𝒂𝒛𝒆𝒖𝒔𝒆.", a: ["pepsi", "coca", "coca cola", "7up", "sprite", "fanta", "schweppes"] },
-  { q: "𝑵𝒐𝒎𝒎𝒆𝒛 𝒖𝒏 𝒍𝒂𝒏𝒈𝒂𝒈𝒆 𝒅𝒆 𝒑𝒓𝒐𝒈𝒓𝒂𝒎𝒎𝒂𝒕𝒊𝒐𝒏.", a: ["actionscript", "coffeescript", "c", "c++", "basic", "python", "perl", "javascript", "dotnet", "lua", "crystal", "go", "d", "php", "ruby", "rust", "dart", "java", "javascript"] },
-  { q: "𝑸𝒖𝒊 𝒆𝒔𝒕 𝒖𝒏 𝒃𝒐𝒏 𝒈𝒂𝒓ç𝒐𝒏?", a: ["Toi", "Alex", "Alexandre", "kawaii alex", "alex", "alexandre", "kawaii Alex", "Kawaii Alex", "Kawaii alex"] },
-  { q: "𝑫𝒂𝒏𝒔 𝒒𝒖𝒆𝒍 𝒍𝒂𝒏𝒈𝒂𝒈𝒆 𝒅𝒆 𝒑𝒓𝒐𝒈𝒓𝒂𝒎𝒎𝒂𝒕𝒊𝒐𝒏 𝒔𝒖𝒊𝒔-𝒋𝒆?", a: ["javascript",] },
-  { q: "𝑵𝒐𝒎𝒎𝒆𝒛 𝒍𝒂 𝒔𝒆𝒑𝒕𝒊𝒆̀𝒎𝒆 𝒑𝒍𝒂𝒏𝒆̀𝒕𝒆 𝒅𝒖 𝒔𝒚𝒔𝒕𝒆̀𝒎𝒆 𝒔𝒐𝒍𝒂𝒊𝒓𝒆.", a: ["uranus"] },
-  { q: "𝑵𝒐𝒎𝒎𝒆𝒛 𝒍𝒂 𝒑𝒍𝒖𝒔 𝒈𝒓𝒂𝒏𝒅𝒆 𝒊̂𝒍𝒆 𝒅𝒖 𝒎𝒐𝒏𝒅𝒆.", a: ["greenland",] },
-  { q: "𝑸𝒖𝒆𝒍 𝒆𝒔𝒕 𝒍𝒆 𝒑𝒍𝒖𝒔 𝒍𝒐𝒏𝒈 𝒇𝒍𝒆𝒖𝒗𝒆 𝒅𝒖 𝒎𝒐𝒏𝒅𝒆?", a: ["amazon", "la rivière amazon"] },
-  { q: "𝑵𝒐𝒎𝒎𝒆𝒛 𝒍𝒆 𝒑𝒍𝒖𝒔 𝒈𝒓𝒂𝒏𝒅 𝒐𝒄𝒆́𝒂𝒏 𝒅𝒖 𝒎𝒐𝒏𝒅𝒆.", a: ["pacifique", "ocean pacifique", "l'ocean pacifique"] },
-  { q: "𝑵𝒐𝒎𝒎𝒆𝒛 𝒍'𝒖𝒏𝒆 𝒅𝒆𝒔 𝒕𝒓𝒐𝒊𝒔 𝒄𝒐𝒖𝒍𝒆𝒖𝒓𝒔 𝒑𝒓𝒊𝒎𝒂𝒊𝒓𝒆𝒔.", a: ["bleu", "rouge", "jaune", "Bleu", "Rouge", "Jaune"] },
-  { q: "𝑪𝒐𝒎𝒃𝒊𝒆𝒏 𝒅𝒆 𝒄𝒐𝒖𝒍𝒆𝒖𝒓𝒔 𝒚 𝒂-𝒕-𝒊𝒍 𝒅𝒂𝒏𝒔 𝒖𝒏 𝒂𝒓𝒄-𝒆𝒏-𝒄𝒊𝒆𝒍?", a: ["7", "sept"] },
-  { q: "𝑪𝒐𝒎𝒃𝒊𝒆𝒏 𝒚 𝒂-𝒕-𝒊𝒍 𝒅𝒆 𝒄𝒂𝒔𝒆𝒔 𝒔𝒖𝒓 𝒖𝒏 𝒆́𝒄𝒉𝒊𝒒𝒖𝒊𝒆𝒓?", a: ["64", "soixante quatre"] },
-  { q: "𝑪𝒐𝒎𝒃𝒊𝒆𝒏 𝒅𝒆 𝒅𝒆𝒈𝒓𝒆́𝒔 𝒕𝒓𝒐𝒖𝒗𝒆-𝒕-𝒐𝒏 𝒅𝒂𝒏𝒔 𝒖𝒏 𝒄𝒆𝒓𝒄𝒍𝒆?", a: ["360", "360 degrées", "trois cent soixante degrées", "trois cent soixante"] },
-  { q: "𝑪𝒊𝒕𝒆𝒓 𝒍𝒆 𝒑𝒓𝒆́𝒏𝒐𝒎 𝒅'𝒖𝒏𝒆 𝒘𝒂𝒊𝒇𝒖 𝒅𝒆 𝑲𝒂𝒘𝒂𝒊𝒊 𝑨𝒍𝒆𝒙", a: ["rem", "Rem", "ochaco", "Ochaco", "ochako", "Ochako", "mirai", "Mirai"] },
-  { q: "𝑪𝒐𝒎𝒃𝒊𝒆𝒏 𝒅𝒆 𝒑𝒐𝒊𝒏𝒕𝒔 𝒖𝒏𝒆 𝒃𝒐𝒖𝒔𝒔𝒐𝒍𝒆 𝒂-𝒕-𝒆𝒍𝒍𝒆?", a: ["32", "trente deux"] },
-  { q: "𝑪𝒐𝒎𝒃𝒊𝒆𝒏 𝒅𝒆 𝒄𝒐𝒓𝒅𝒆𝒔 𝒂 𝒖𝒏 𝒗𝒊𝒐𝒍𝒐𝒏𝒄𝒆𝒍𝒍𝒆?", a: ["4", "quatre"] },
-  { q: "𝑪𝒐𝒎𝒃𝒊𝒆𝒏 𝒅𝒆 𝒔𝒚𝒎𝒑𝒉𝒐𝒏𝒊𝒆𝒔 𝒂 𝒄𝒐𝒎𝒑𝒐𝒔𝒆́𝒆𝒔 𝑩𝒆𝒆𝒕𝒉𝒐𝒗𝒆𝒏?", a: ["9", "neuf"] },
-  { q: "𝑸𝒖'𝒆𝒍𝒍𝒆 𝒂̂𝒈𝒆 𝒂 𝑲𝒂𝒘𝒂𝒊𝒊 𝑨𝒍𝒆𝒙 𝒎𝒂𝒊𝒏𝒕𝒆𝒏𝒂𝒏𝒕", a: ["14", "quatorze"] },
-  { q: "𝑸𝒖𝒆𝒍 𝒆𝒔𝒕 𝒍𝒆 𝒍𝒂𝒏𝒈𝒂𝒈𝒆 𝒍𝒆 𝒑𝒍𝒖𝒔 𝒃𝒂𝒔𝒊𝒒𝒖𝒆 𝒅𝒆 𝑴𝒊𝒄𝒓𝒐𝒔𝒐𝒇𝒕?", a: ["visual basic"] },
-  { q: "𝑸𝒖𝒆𝒍𝒍𝒆 𝒆𝒔𝒕 𝒍𝒆 𝒍𝒂𝒏𝒈𝒖𝒂𝒈𝒆 𝒍𝒂 𝒑𝒍𝒖𝒔 𝒄𝒐𝒎𝒑𝒍𝒊𝒒𝒖𝒆́𝒆?", a: ["binaire", "le language binaire"] },
-  { q: "«𝑶𝑺/𝑺𝑬» 𝒂𝒃𝒓𝒆́𝒗𝒊𝒂𝒕𝒊𝒐𝒏 𝒊𝒏𝒇𝒐𝒓𝒎𝒂𝒕𝒊𝒒𝒖𝒆 𝒔𝒊𝒈𝒏𝒊𝒇𝒊𝒆 𝒈𝒆́𝒏𝒆́𝒓𝒂𝒍𝒆𝒎𝒆𝒏𝒕?", a: ["operating system", "Système d’exploitation", "Operating system", "système d'exploitation", "systeme d'exploitation", "Systeme d'exploitation"] }
+  { q: "De qu'elle couleur est le ciel?", a: ["Pas de couleur", "invisible"] },
+  { q: "Nommez une marque de boisson gazeuse.", a: ["pepsi", "coca", "coca cola", "7up", "sprite", "fanta", "schweppes"] },
+  { q: "Nommez un langage de programmation.", a: ["actionscript", "coffeescript", "c", "c++", "basic", "python", "perl", "javascript", "dotnet", "lua", "crystal", "go", "d", "php", "ruby", "rust", "dart", "java", "javascript"] },
+  { q: "Qui est un bon garçon?", a: ["Toi", "Alex", "Alexandre", "kawaii alex", "alex", "alexandre", "kawaii Alex", "Kawaii Alex", "Kawaii alex"] },
+  { q: "Dans quel langage de programmation suis-je?", a: ["javascript",] },
+  { q: "Nommez la septième planète du système solaire.", a: ["uranus"] },
+  { q: "Nommez la plus grande île du monde.", a: ["greenland",] },
+  { q: "Quel est le plus long fleuve du monde?", a: ["amazon", "la rivière amazon"] },
+  { q: "Nommez le plus grand océan du monde.", a: ["pacifique", "ocean pacifique", "l'ocean pacifique"] },
+  { q: "Nommez l'une des trois couleurs du monde.", a: ["bleu", "rouge", "jaune", "Bleu", "Rouge", "Jaune"] },
+  { q: "Combien de couleurs y a-t-il dans un arc-en-ciel?", a: ["7", "sept"] },
+  { q: "Combien y a-t-il de case dans un échiquier?", a: ["64", "soixante quatre"] },
+  { q: "Combien de degrés trouve t'on dans un cercle?", a: ["360", "360 degrées", "trois cent soixante degrées", "trois cent soixante"] },
+  { q: "Citer le prénom d'une wiafu de Kawaii Alex", a: ["rem", "Rem", "ochaco", "Ochaco", "ochako", "Ochako", "mirai", "Mirai"] },
+  { q: "Combien de points une boussole a-t-elle?", a: ["32", "trente deux"] },
+  { q: "Combien de corde a un violoncelle?", a: ["4", "quatre"] },
+  { q: "Combien de symphonies a composées Beethoven ?", a: ["9", "neuf"] },
+  { q: "Qu'elle âge a Kawaii Alex maintenant ?", a: ["14", "quatorze"] },
+  { q: "Quel est le langage le plus basique de Microsoft ?", a: ["visual basic"] },
+  { q: "Qu'elle est le langage le plus compliquée ?", a: ["binaire", "le language binaire"] },
+  { q: "«OS/SE» abrévation informatique signifie généralement ?", a: ["operating system", "Système d’exploitation", "Operating system", "système d'exploitation", "systeme d'exploitation", "Systeme d'exploitation"] }
 ];
 const options = {
   max: 1,
@@ -37,18 +37,18 @@ exports.run = async (bot, message, args) => {
     const collected = await message.channel.awaitMessages(answer => item.a.includes(answer.content.toLowerCase()), options);
     const winnerMessage = collected.first();
     const embed = new Discord.RichEmbed()
-    .setAuthor(`𝑮𝒂𝒈𝒏𝒂𝒏𝒕: ${winnerMessage.author.tag}`, winnerMessage.author.displayAvatarURL)
-    .setTitle(`𝑩𝒐𝒏𝒏𝒆 𝒓𝒆́𝒑𝒐𝒏𝒔𝒆: \`${winnerMessage.content}\``)
-    .setFooter(`𝑸𝒖𝒆𝒔𝒕𝒊𝒐𝒏: ${item.q}`)
+    .setAuthor(`Gagnant: ${winnerMessage.author.tag}`, winnerMessage.author.displayAvatarURL)
+    .setTitle(`Bonne réponse: \`${winnerMessage.content}\``)
+    .setFooter(`Question: ${item.q}`)
     .setColor(message.guild.me.displayHexColor)
-    if(args[0] !== winnerMessage.content) return message.channel.send("𝑩𝑶𝑵𝑵𝑬 𝑹𝑬𝑷𝑶𝑵𝑺𝑬 !") + message.channel.send(embed)
+    if(args[0] !== winnerMessage.content) return message.channel.send("BONNE REPONSE ヽ༼ຈل͜ຈ༽ﾉ !") + message.channel.send(embed)
 
   } catch (_) {
 
     const embedtime = new Discord.RichEmbed()
-                                 .setAuthor('𝑷𝒆𝒓𝒔𝒐𝒏𝒏𝒆 𝒏\'𝒂 𝒆𝒖 𝒍𝒂 𝒓𝒆́𝒑𝒐𝒏𝒔𝒆 𝒂̀ 𝒕𝒆𝒎𝒑𝒔!')
-                                 .setTitle(`𝑩𝒐𝒏𝒏𝒆 𝒓𝒆́𝒑𝒐𝒏𝒔𝒆: \`${item.a}\``)
-                                 .setFooter(`𝑸𝒖𝒆𝒔𝒕𝒊𝒐𝒏: ${item.q}`)
+                                 .setAuthor('Personne n\'a eu la réponse à temps ヽ(ヅ)ノ !')
+                                 .setTitle(`Bonne réponse: \`${item.a}\``)
+                                 .setFooter(`Question: ${item.q}`)
     return message.channel.send(embedtime)
   }
 }
