0c604c3cd148caf60c526ecd1ef5c05c0dd0032f
KawaiiAlex <38259844+KawaiiAlex@users.noreply.github.com>
2018-06-29 20:10:04 +0200

    Update avatar.js

--- commands/Fun/avatar.js
+++ commands/Fun/avatar.js
@@ -31,7 +31,7 @@ exports.run = (client, message, args) => {
     
     message.channel.send({embed: new Discord.RichEmbed()
                           .setImage(definedUser.avatarURL)
-                          .setDescription(`[Avatar de ${definedUser.tag}](${definedUser.avatarURL})`)
+                          .setTitle(`Avatar de ${definedUser.tag}`)
                           .setColor(`${message.guild.me.displayHexColor!=='#00000' ? message.guild.me.displayHexColor : 0xffffff}`)
                           .setFooter(client.user.username, client.user.displayAvatarURL).setTimestamp()
                          })
